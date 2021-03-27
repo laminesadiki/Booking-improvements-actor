92dcd57badb5f856ef14323d744ae48f90746fb8
sadiki-dev <sadiki.dev@gmail.com>
2021-03-27 19:30:20 +0100

    surround categories with try/catch

--- src/extraction.js
+++ src/extraction.js
@@ -123,18 +123,23 @@ module.exports.extractDetail = async (page, ld, input, userData) => {
 
     // Add categories Object == {title : scrore}
     const categories = await page.evaluate(() => {
-    let categoriesElement = document.querySelector("div[class='v2_review-scores__body v2_review-scores__body--compared_to_average']");
-    let categoryList = [...categoriesElement.querySelectorAll("li")]
+        try {
+            let categoriesElement = document.querySelector("div[class='v2_review-scores__body v2_review-scores__body--compared_to_average']");
+            let categoryList = [...categoriesElement.querySelectorAll("li")]
 
-    let CategoriesList  =categoryList.map( el => {
-        let title = el.querySelector("span.c-score-bar__title").innerText.trim();
-        let score = el.querySelector("span.c-score-bar__score").innerText;
-        return {[title] : score};
-    })
+            let CategoriesList  =categoryList.map( el => {
+                let title = el.querySelector("span.c-score-bar__title").innerText.trim();
+                let score = el.querySelector("span.c-score-bar__score").innerText;
+                    return {[title] : score};
+            })
 
-    let categoriesObj = Object.assign({},...CategoriesList);
+            let categoriesObj = Object.assign({},...CategoriesList);
 
-    return categoriesObj;
+            return categoriesObj;
+        } catch (error) {
+            return "";
+        }
+    
     });
 
     
